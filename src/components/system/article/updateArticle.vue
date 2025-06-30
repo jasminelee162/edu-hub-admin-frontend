0db4445b5e4f5b4bb6906e6c9b6eef0c5c03c30f
Fiesta <3204986849@qq.com>
2025-06-30 20:09:13 +0800

    笔记章节部分修改

--- src/components/system/article/updateArticle.vue
+++ src/components/system/article/updateArticle.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
-  <el-dialog title="编辑笔记" width="40%" :destroy-on-close="true" :visible.sync="updateVisible" :before-close="handleClose">
+  <el-dialog title="编辑笔记" width="40%" :destroy-on-close="true" :visible.sync="updateVisible" :before-close="handleClose" class="custom-dialog">
     <el-form :model="form" :rules="rules" ref="ruleForm">
       <el-row :gutter="10">
         <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <span class="search-title">
-                所属课程:
-                </span>
+                <i class="el-icon-notebook-1"></i> 所属课程:
+            </span>
                 <div style="width:100%">
                     <el-form-item prop="taskId" style="margin-bottom:0">
-                        <el-select :disabled="type == 0 ? true:false" clearable style="margin-top:10px" size="mini" v-model="form.taskId" placeholder="请选择">
+                        <el-select :disabled="type == 0 ? true:false" clearable style="margin-top:10px" size="mini" v-model="form.taskId" placeholder="请选择" class="purple-select">
                             <el-option v-for="(item,index) in task" :key="index"
                                 :label="item.name"
                                 :value="item.id">
@@ -20,32 +20,35 @@
         </el-col>
         <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <span class="search-title">
-                标题:
-                </span>
+                <i class="el-icon-edit-outline"></i> 标题:
+            </span>
                 <div style="width:100%">
                   <el-form-item prop="title" style="margin-bottom:0">
-                      <el-input v-model="form.title" size="mini" placeholder="请输入标题" autocomplete="off"></el-input>
+                      <el-input v-model="form.title" size="mini" placeholder="请输入标题" autocomplete="off" class="purple-input">
+                        <i slot="prefix" class="el-icon-edit"></i>
+                      </el-input>
                   </el-form-item>
                 </div>
         </el-col>
         <el-col style="margin-top:25px" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <span class="search-title">
-                摘要:
-                </span>
+                <i class="el-icon-document"></i> 摘要:
+            </span>
                 <div style="width:100%">
                   <el-form-item prop="articleDesc" style="margin-bottom:0">
-                      <el-input type="textarea" v-model="form.articleDesc" size="mini" placeholder="请输入摘要" autocomplete="off"></el-input>
+                      <el-input type="textarea" v-model="form.articleDesc" size="mini" placeholder="请输入摘要" autocomplete="off" class="purple-textarea">
+                      </el-input>
                   </el-form-item>
                 </div>
         </el-col>
         <el-col style="margin-top:25px" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <span class="search-title">
-                内容:
-                </span>
+                <i class="el-icon-tickets"></i> 内容:
+            </span>
                 <div style="width:100%">
                   <el-form-item prop="content" style="margin-bottom:0">
                     <quill-editor ref="myQuillEditor" :options="editorOption"
-                    v-model="form.content"/>
+                    v-model="form.content" class="purple-editor"/>
                     <el-upload class="Quill-uploader" :show-file-list="false" accept="image/*" v-show="0" ref="updateUpload" :multiple="false" :action="uploadImageUrl()"
                         :on-success="handleQillSuccess">
                     </el-upload>
@@ -54,19 +57,24 @@
         </el-col>
         <el-col style="margin-top:25px" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <span class="search-title">
-                备注:
-                </span>
+                <i class="el-icon-collection-tag"></i> 备注:
+            </span>
                 <div style="width:100%">
                   <el-form-item prop="remark" style="margin-bottom:0">
-                      <el-input type="textarea" v-model="form.remark" size="mini" placeholder="请输入备注" autocomplete="off"></el-input>
+                      <el-input type="textarea" v-model="form.remark" size="mini" placeholder="请输入备注" autocomplete="off" class="purple-textarea">
+                      </el-input>
                   </el-form-item>
                 </div>
         </el-col>
       </el-row>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button size="mini" type="primary" @click="submit">确 定</el-button>
-      <el-button size="mini" @click="handleClose">取 消</el-button>
+      <el-button size="mini" type="primary" @click="submit" class="purple-btn">
+        <i class="el-icon-check"></i> 确 定
+      </el-button>
+      <el-button size="mini" @click="handleClose" class="gray-btn">
+        <i class="el-icon-close"></i> 取 消
+      </el-button>
     </div>
   </el-dialog>
 </div>
@@ -267,8 +275,92 @@
       font-size: 14px;
       width: 84px;
       text-align: right;
+      color: #5F4B8B;
+  }
+  .search-title i {
+      margin-right: 6px;
+      color: #7B68EE;
   }
   .el-tree {
       border: 1px solid #BDC1C9;
+  }
+</style>
+
+<style>
+  /* 全局样式 */
+  .custom-dialog .el-dialog__header {
+      background: linear-gradient(135deg, #7B68EE, #9370DB);
+      padding: 15px 20px;
+  }
+  
+  .custom-dialog .el-dialog__title {
+      color: white;
+      font-weight: bold;
+  }
+  
+  .custom-dialog .el-dialog__headerbtn .el-dialog__close {
+      color: white;
+  }
+  
+  .purple-select .el-input__inner {
+      border-color: #D8D8E5;
+      color: #5F4B8B;
+      background-color: #F9F8FD;
+  }
+  
+  .purple-select .el-input__inner:focus {
+      border-color: #7B68EE;
+  }
+  
+  .purple-input .el-input__inner {
+      border-color: #D8D8E5;
+      color: #5F4B8B;
+      background-color: #F9F8FD;
+      padding-left: 30px;
+  }
+  
+  .purple-input .el-input__prefix {
+      left: 5px;
+      color: #7B68EE;
+  }
+  
+  .purple-textarea .el-textarea__inner {
+      border-color: #D8D8E5;
+      color: #5F4B8B;
+      background-color: #F9F8FD;
+      min-height: 80px !important;
+  }
+  
+  .purple-editor .ql-toolbar {
+      border-color: #D8D8E5 !important;
+      background-color: #F5F2FF;
+  }
+  
+  .purple-editor .ql-container {
+      border-color: #D8D8E5 !important;
+      height: 200px;
+  }
+  
+  .purple-btn {
+      background: linear-gradient(135deg, #7B68EE, #9370DB);
+      border: none;
+      color: white;
+      border-radius: 15px;
+      padding: 7px 15px;
+  }
+  
+  .purple-btn i {
+      margin-right: 5px;
+  }
+  
+  .gray-btn {
+      border-color: #D8D8E5;
+      color: #5F4B8B;
+      border-radius: 15px;
+      padding: 7px 15px;
+  }
+  
+  .gray-btn i {
+      margin-right: 5px;
   }
 </style>
